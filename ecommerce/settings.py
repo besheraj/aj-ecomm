5120fecbaa7f434c95b05d58bdc914f6f0405349
Behser Aj <besher.aj@outlook.com>
2020-10-28 16:49:40 +0800

    fixing deployment

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('debug')
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['aj-webecom.herokuapp.com','locathost']
 
