bf31f97cd26c09cf69d9061c24103b2f00f7fdd1
Behser Aj <besher.aj@outlook.com>
2020-11-01 20:35:07 +0800

    added the allauth to setting and added home app

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['aj-webecom.herokuapp.com','locathost',]
+ALLOWED_HOSTS = ['aj-webecom.herokuapp.com','locathost']
 
 
 # Application definition
