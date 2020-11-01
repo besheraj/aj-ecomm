cca7e9e2d728a63320db711c0be96d12e1339504
Behser Aj <besher.aj@outlook.com>
2020-11-01 16:45:05 +0800

    re style the main page

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG= True
 
-ALLOWED_HOSTS = ['aj-webecom.herokuapp.com','locathost']
+ALLOWED_HOSTS = ['aj-webecom.herokuapp.com','locathost','127.0.0.1']
 
 
 # Application definition
