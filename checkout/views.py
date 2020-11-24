2602f92a03fff01772067038439633ca06c6b1de
Behser Aj <besher.aj@outlook.com>
2020-11-24 15:09:43 +0800

    code refactor

--- checkout/views.py
+++ checkout/views.py
@@ -35,6 +35,15 @@ def cache_checkout_data(request):
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
 
     if request.method == 'POST':
         bag = request.session.get('bag', {})
