e6dc19a8925f0a2c9c569c1e96da96ae866f4e8c
Besher <besher@Beshers-MacBook-Pro.local>
2021-02-26 00:05:48 +0800

    changed the host name

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['aj-webecom.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['aj-ecomm.herokuapp.com','127.0.0.1']
 
 
 # Application definition
